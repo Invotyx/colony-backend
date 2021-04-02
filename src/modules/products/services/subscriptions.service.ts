d5f39be0c8cc8d53bb7b806e49f00348e82f6e00
Danyal <danyalr8@gmail.com>
2021-04-02 15:26:12 +0500

    changes in subscription api - bug fix

--- src/modules/products/services/subscriptions.service.ts
+++ src/modules/products/services/subscriptions.service.ts
@@ -131,7 +131,7 @@ export class SubscriptionsService {
 
   public async getSubscriptions(customer: UserEntity) {
     try {
-      const ch =  await this.repository.query(`SELECT sub.*,  phones."number", "country"."name" FROM "subscriptions" "sub" LEFT JOIN "plans" "plans" ON "plans"."id"="sub"."planId" 
+      const ch =  await this.repository.query(`SELECT sub.*,  phones."number","plans"."nickname" as "planName", "country"."name" as "countryName"  FROM "subscriptions" "sub" LEFT JOIN "plans" "plans" ON "plans"."id"="sub"."planId" 
       LEFT JOIN "country" "country" ON "country"."id"="sub"."countryId"  LEFT JOIN "phones" "phones" ON "phones"."id"="sub"."phoneId" WHERE ( "sub"."userId" =${customer.id}  ) AND ( "sub"."deletedAt" IS NULL )`);
       //console.log(ch);
 

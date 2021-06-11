063fdbd98635a5fb2165a933a7aeac309a559a8e
Danyal <danyalr8@gmail.com>
2021-06-11 11:24:57 +0500

    jwt secrect fixed

--- src/consts/jwt.const.ts
+++ src/consts/jwt.const.ts
@@ -1,5 +1,4 @@
-import { nanoid } from 'nanoid';
-
 export const jwtConstants = {
-  secret: nanoid(),
+  secret:
+    '9WoqZguA3hd1NuVQf10JQD053dXGiR6qQ8vebg4MRMUyNYinL3hUMTOhjjXmjkn2sG6dPUzvQrLfKQZ7',
 };

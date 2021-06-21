5e1e95d446dd2e88a0c0219b806dad5705a5774e
Danyal <danyalr8@gmail.com>
2021-06-21 12:54:52 +0500

    bug fix -- where

--- src/seeder/seeds/country.seed.ts
+++ src/seeder/seeds/country.seed.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CityCountryService } from '../../services/city-country/city-country.service';
+import * as fs from 'fs';
 import { Seeder } from '../../decorators/common.decorator';
+import { CityCountryService } from '../../services/city-country/city-country.service';
 import { ISeed } from '../seeds.interface';
 
 @Injectable()
@@ -8,7 +9,24 @@ import { ISeed } from '../seeds.interface';
 export class CountrySeed implements ISeed {
   constructor(private readonly service: CityCountryService) {}
   async up() {
-    await this.service.country();
+    const obj = JSON.parse(fs.readFileSync('geo_location.json', 'utf8'));
+
+    for (const item of obj) {
+      const id = item.Id;
+      const name = item.Name;
+      // insert country here;
+      await this.service.countryRepo.save({
+        id: id,
+        code: id,
+        name: name
+      })
+      for (const city of item.Cities) {
+        const [lat, long] = (city.Location as string).split(',');
+        const IataCode = city.IataCode;
+        const CityName = city.Name;
+        //insert city here;
+      }
+    }
   }
   async down() {
     //

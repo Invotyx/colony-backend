3c50cd5b75fc574aed0b3088d77b5c24b81041e1
Danyal <danyalr8@gmail.com>
2021-07-05 16:29:31 +0500

    bug fix link

--- src/shared/tag-replace.ts
+++ src/shared/tag-replace.ts
@@ -1,3 +1,6 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { error } from './error.dto';
+
 /**
  * Convert a literal string to dynamically add variables passed in params object
  * @param {String} literal Literal string having named variables as ${variable}
@@ -11,6 +14,22 @@
  * @returns {String} Returns plan string after inserting dynamic names
  */
 export const tagReplace = function template(literal, params) {
+  if (String(literal).includes(':')) {
+    throw new HttpException(
+      error(
+        [
+          {
+            key: 'body',
+            reason: 'invalidMergeTag',
+            description: 'You have used invalid merge tag.',
+          },
+        ],
+        HttpStatus.UNPROCESSABLE_ENTITY,
+        'Unprocessable merge tag',
+      ),
+      HttpStatus.UNPROCESSABLE_ENTITY,
+    );
+  }
   return new Function(Object.keys(params) as any, 'return `' + literal + '`;')(
     ...Object.values(params),
   );

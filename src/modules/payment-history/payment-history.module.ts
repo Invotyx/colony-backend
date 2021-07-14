73e96b64ff11ebdfb1f6ecdb094eef66cebd366b
Danyal <danyalr8@gmail.com>
2021-07-14 13:50:55 +0500

    queues fix

--- src/modules/payment-history/payment-history.module.ts
+++ src/modules/payment-history/payment-history.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MainMysqlModule } from 'src/shared/main-mysql.module';
 import { PaymentsModule } from '../products/payments/payments.module';
 import { PlanModule } from '../products/plan/plan.module';
+import { ProductsModule } from '../products/products.module';
 import { PaymentHistoryController } from './payment-history.controller';
 import { PaymentHistoryService } from './payment-history.service';
 
 @Module({
-  imports: [MainMysqlModule, PlanModule, PaymentsModule],
+  imports: [MainMysqlModule, ProductsModule, PlanModule, PaymentsModule],
   controllers: [PaymentHistoryController],
   providers: [PaymentHistoryService],
   exports: [PaymentHistoryService],

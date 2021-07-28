96354a3b6014985584575126adba084db2967ee5
Danyal <danyalr8@gmail.com>
2021-07-28 14:25:06 +0500

    redirection fix

--- src/modules/shareable-link-handler/shareable-link-handler.controller.ts
+++ src/modules/shareable-link-handler/shareable-link-handler.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Redirect, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { ShareableLinkHandlerService } from './shareable-link-handler.service';
 
@@ -7,10 +7,11 @@ export class ShareableLinkHandlerController {
   constructor(private readonly service: ShareableLinkHandlerService) {}
 
   @Get('o/:id')
+  @Redirect()
   public async linkOpened(@Param('id') id: string, @Res() res: Response) {
     try {
       const link = await this.service.linkOpened(id);
-      res.redirect(link);
+      return { url: link, statusCode: 301 };
     } catch (e) {
       throw e;
     }

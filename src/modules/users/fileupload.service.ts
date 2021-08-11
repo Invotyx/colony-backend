d3423bd416ad462a48d9f107d3d8af81b3c38a40
danyal-903 <danyal@invotyx.com>
2021-08-11 14:41:40 +0500

    voice changes

--- src/modules/users/fileupload.service.ts
+++ src/modules/users/fileupload.service.ts
@@ -1,5 +1,6 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { extname } from 'path';
+import { uniqueId } from 'src/shared/random-keygen';
 
 export const imageFileFilter = (req: any, file: any, callback: any) => {
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
@@ -29,9 +30,6 @@ export const audioFileFilter = (req: any, file: any, callback: any) => {
 export const editFileName = (req: any, file: any, callback: any) => {
   const name = file.originalname.split('.')[0];
   const fileExtName = extname(file.originalname);
-  const randomName = Array(4)
-    .fill(null)
-    .map(() => Math.round(Math.random() * 10).toString(10))
-    .join('');
-  callback(null, `${name}${randomName}${fileExtName}`);
+  const randomName = uniqueId(6);
+  callback(null, `audio-${randomName}${fileExtName}`);
 };
